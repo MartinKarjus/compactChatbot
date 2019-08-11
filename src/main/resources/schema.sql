64656f1795d280037278bad567d7ae06c3de6d08
Rain Vagel <rain.vagel@gmail.com>
2019-08-11 13:52:52 +0300

    Merged question tables to one. Added date_created and date_modified fields. Added a transmission log to know if we have sent to people or not

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -33,21 +33,29 @@ CREATE
     SEQUENCE plan_accomplished_sequence AS INTEGER START
     WITH 1;
 CREATE
-    SEQUENCE answers_log_sequence AS INTEGER START
+    SEQUENCE answers_sequence AS INTEGER START
+    WITH 1;
+
+CREATE
+    SEQUENCE transmission_log_sequence AS INTEGER START
     WITH 1;
 
 
 CREATE TABLE main.question_group (
    id BIGINT NOT NULL PRIMARY KEY,
    name VARCHAR(250) NOT NULL,
-   company BIGINT,
-   FOREIGN KEY (company)
-     REFERENCES main.company on DELETE CASCADE
+   date_created TIMESTAMP,
+   date_modified TIMESTAMP,
+   company_id BIGINT,
+   FOREIGN KEY (company_id)
+     REFERENCES main.company (id) on DELETE CASCADE
 );
 
 CREATE TABLE main.media
 (
     id     BIGINT NOT NULL PRIMARY KEY,
+    date_created TIMESTAMP,
+    date_modified TIMESTAMP,
     type   VARCHAR(5000),
     source VARCHAR(5000)
 );
@@ -55,61 +63,69 @@ CREATE TABLE main.media
 CREATE TABLE main.question
 (
     id                     BIGINT NOT NULL PRIMARY KEY,
+    date_created           TIMESTAMP,
+    date_modified          TIMESTAMP,
+    name                   VARCHAR(300),
+    description            VARCHAR(500),
     text                   VARCHAR(5000),
     can_be_random_selected BOOLEAN DEFAULT FALSE,
+    leads_to_question_id   BIGINT,
     media_id               BIGINT,
     FOREIGN KEY (media_id)
         REFERENCES main.media (id) ON DELETE CASCADE
 );
 
-CREATE TABLE main.content
-(
-    id                   BIGINT NOT NULL PRIMARY KEY,
-    leads_to_content_id  BIGINT,
-    leads_to_question_id BIGINT,
-    text                 VARCHAR(5000),
-    media_id             BIGINT,
-    FOREIGN KEY (leads_to_content_id)
-        REFERENCES main.content (id) ON DELETE CASCADE,
-    FOREIGN KEY (media_id)
-        REFERENCES main.media (id) ON DELETE CASCADE,
-    FOREIGN KEY (leads_to_question_id)
-        REFERENCES main.question (id) ON DELETE CASCADE
-);
+-- CREATE TABLE main.content
+-- (
+--     id                   BIGINT NOT NULL PRIMARY KEY,
+--     leads_to_content_id  BIGINT,
+--     leads_to_question_id BIGINT,
+--     text                 VARCHAR(5000),
+--     media_id             BIGINT,
+--     FOREIGN KEY (leads_to_content_id)
+--         REFERENCES main.content (id) ON DELETE CASCADE,
+--     FOREIGN KEY (media_id)
+--         REFERENCES main.media (id) ON DELETE CASCADE,
+--     FOREIGN KEY (leads_to_question_id)
+--         REFERENCES main.question (id) ON DELETE CASCADE
+-- );
 
-CREATE TABLE main.question_query
-(
-    id                  BIGINT NOT NULL PRIMARY KEY,
-    query_text          VARCHAR(5000),
-    query_response      VARCHAR(5000),
-    leads_to_content_id BIGINT,
-    media_id            BIGINT,
-    question_id         BIGINT,
-    FOREIGN KEY (question_id)
-        REFERENCES main.question (id) ON DELETE CASCADE,
-    FOREIGN KEY (leads_to_content_id)
-        REFERENCES main.content (id) ON DELETE CASCADE,
-    FOREIGN KEY (media_id)
-        REFERENCES main.media (id) ON DELETE CASCADE
-);
+-- CREATE TABLE main.question_query
+-- (
+--     id                  BIGINT NOT NULL PRIMARY KEY,
+--     query_text          VARCHAR(5000),
+--     query_response      VARCHAR(5000),
+--     leads_to_content_id BIGINT,
+--     media_id            BIGINT,
+--     question_id         BIGINT,
+--     FOREIGN KEY (question_id)
+--         REFERENCES main.question (id) ON DELETE CASCADE,
+--     FOREIGN KEY (leads_to_content_id)
+--         REFERENCES main.content (id) ON DELETE CASCADE,
+--     FOREIGN KEY (media_id)
+--         REFERENCES main.media (id) ON DELETE CASCADE
+-- );
 
 CREATE TABLE main.time_to_send
 (
     id                BIGINT NOT NULL PRIMARY KEY,
-    time_to_send  DATE
+    date_created      TIMESTAMP,
+    time_to_send      DATE
 );
 
 CREATE TABLE main.plan
 (
     id              BIGINT NOT NULL PRIMARY KEY,
-    content_id      BIGINT,
+    date_created    TIMESTAMP,
+    date_modified   TIMESTAMP,
+    question_id      BIGINT,
     time_to_send_id BIGINT,
     day             BIGINT,
     company_id      BIGINT,
     FOREIGN KEY (company_id)
         REFERENCES main.company (id) ON DELETE CASCADE,
-    FOREIGN KEY (content_id)
-        REFERENCES main.content (id) ON DELETE CASCADE,
+    FOREIGN KEY (question_id)
+        REFERENCES main.question (id) ON DELETE CASCADE,
     FOREIGN KEY (time_to_send_id)
         REFERENCES main.time_to_send (id) ON DELETE CASCADE
 );
@@ -118,13 +134,14 @@ CREATE TABLE main.user (
      id BIGINT NOT NULL PRIMARY KEY,
      first_name VARCHAR(200),
      last_name VARCHAR(200),
+     date_created TIMESTAMP,
+     date_modified TIMESTAMP,
      gender VARCHAR(10),
      birtdate TIMESTAMP,
      email VARCHAR(200),
      team_id BIGINT,
      company BIGINT NOT NULL,
      score BIGINT,
-     join_date DATE,
      question_group_id BIGINT NOT NULL,
      FOREIGN KEY (question_group_id)
        REFERENCES main.question_group ON DELETE CASCADE,
@@ -137,17 +154,21 @@ CREATE TABLE main.user (
 CREATE TABLE main.team (
    id BIGINT NOT NULL PRIMARY KEY,
    name VARCHAR(200) NOT NULL,
+   date_created TIMESTAMP,
+   date_modified TIMESTAMP,
    score BIGINT
 );
 
 CREATE TABLE main.company (
   id BIGINT NOT NULL PRIMARY KEY,
+  date_created TIMESTAMP,
   name VARCHAR(200)
 );
 
 CREATE TABLE main.plan_accomplished
 (
     id           BIGINT NOT NULL PRIMARY KEY,
+    date_created TIMESTAMP,
     user_id      BIGINT,
     plan_id      BIGINT,
     FOREIGN KEY (user_id)
@@ -156,17 +177,30 @@ CREATE TABLE main.plan_accomplished
         REFERENCES main.plan (id) ON DELETE CASCADE
 );
 
-CREATE TABLE main.answers_log
+CREATE TABLE main.transmission_log
 (
-    id                BIGINT NOT NULL,
-    question_query_id BIGINT,
+  id      BIGINT NOT NULL primary key,
+  question_id BIGINT,
+  date_created TIMESTAMP,
+  user_id BIGINT,
+  state VARCHAR(250),
+  FOREIGN KEY (user_id)
+    REFERENCES main.user (id) on DELETE CASCADE,
+  FOREIGN KEY (question_id)
+    REFERENCES main.question (id) ON DELETE CASCADE
+);
+
+CREATE TABLE main.answers
+(
+    id                BIGINT NOT NULL PRIMARY KEY,
+    question_id BIGINT,
     user_id           BIGINT,
     points            BIGINT,
     answer            VARCHAR(250),
     FOREIGN KEY (user_id)
         REFERENCES main.user (id) ON DELETE CASCADE,
-    FOREIGN KEY (question_query_id)
-        REFERENCES main.question_query (id) ON DELETE CASCADE
+    FOREIGN KEY (question_id)
+        REFERENCES main.question (id) ON DELETE CASCADE
 );
 
 CREATE TABLE main.platform(
